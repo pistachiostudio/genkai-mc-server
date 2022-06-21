1b5541dba2dac655defb99228f364a8dfd31ba45
zztkm <takumi.basket1682@gmail.com>
2022-06-21 23:36:05 +0900

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ curl -O https://raw.githubusercontent.com/MICKeyzwo/minecraft-log-forwarder/mast
 cat example.log_forwarder_config.json | sed -e "s|DISCORD_WEBHOOK_URL|$DISCORD_WEBHOOK_URL|" > log_forwarder_config.json
 
 # setup systemd service
+sudo systemctl disable --now minecraft-log-forwarder.service
 sudo rm -f /etc/systemd/system/minecraft-*
 sudo ln -s `pwd`/minecraft-log-forwarder.service "/etc/systemd/system/"
 sudo systemctl daemon-reload
