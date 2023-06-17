64385f8352d54643b594197f488a77cf6ac92d5d
zztkm <zztkm@tsurutatakumi.info>
2023-06-17 11:05:20 +0900

    良い感じにできた

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@
 curl -O https://raw.githubusercontent.com/MICKeyzwo/minecraft-log-forwarder/master/logForwarder.ts
 cat example.log_forwarder_config.json | sed -e "s|DISCORD_WEBHOOK_URL|$DISCORD_WEBHOOK_URL|" > log_forwarder_config.json
 
+cp example.agent.yml agent.yml
+
 # setup systemd service
 sudo systemctl disable --now minecraft-log-forwarder.service
 sudo rm -f /etc/systemd/system/minecraft-*
