fa5c2dc3012bcd9dda23a15d5bcf4b03b370a641
zztkm <zztkm@tsurutatakumi.info>
2024-06-11 23:09:11 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # setup log forwarder
-curl -O https://raw.githubusercontent.com/MICKeyzwo/minecraft-log-forwarder/master/logForwarder.ts
-cat example.log_forwarder_config.json | sed -e "s|DISCORD_WEBHOOK_URL|$DISCORD_WEBHOOK_URL|" > log_forwarder_config.json
+# curl -O https://raw.githubusercontent.com/MICKeyzwo/minecraft-log-forwarder/master/logForwarder.ts
+# cat example.log_forwarder_config.json | sed -e "s|DISCORD_WEBHOOK_URL|$DISCORD_WEBHOOK_URL|" > log_forwarder_config.json
 
-cp example.agent.yml agent.yml
+# cp example.agent.yml agent.yml
 
 # setup systemd service
-sudo systemctl disable --now minecraft-log-forwarder.service
+# sudo systemctl disable --now minecraft-log-forwarder.service
 sudo systemctl disable --now minecraft-restart.timer
 sudo rm -f /etc/systemd/system/minecraft-*
-sudo ln -s `pwd`/minecraft-log-forwarder.service "/etc/systemd/system/"
+# sudo ln -s `pwd`/minecraft-log-forwarder.service "/etc/systemd/system/"
 sudo ln -s `pwd`/minecraft-restart.service "/etc/systemd/system/"
 sudo ln -s `pwd`/minecraft-restart.timer "/etc/systemd/system/"
+
 sudo systemctl daemon-reload
 sudo systemctl enable --now minecraft-log-forwarder.service
 sudo systemctl enable --now minecraft-restart.timer
+
